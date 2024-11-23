7e0a2fbbf8bf4665e7c96952323d4e5fd1be0a97
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-11-24 04:45:30 +0530

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -5,4 +5,4 @@ export const DONATION_LINK = "https://ko-fi.com/vidbinge";
 export const FACEBOOK_LINK = "https://facebook.com/vidbinge";
 export const INSTAGRAM_LINK = "https://instagram.com/vidbinge";
 export const GA_ID = import.meta.env.VITE_GA_ID;
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const BACKEND_URL = "vidbinge.com";
